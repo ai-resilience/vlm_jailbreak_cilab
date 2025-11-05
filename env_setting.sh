4ffd730d009a9c7f08947825b1bc985277905301
Wonjun-Lee1009 <dpenguin2000@gmail.com>
2025-11-05 21:14:59 +0900

    env setting - deepseekvl2 no need to make more env - download transformers 4.38.2 into deepseekvl2

---
+++ env_setting.sh
@@ -0,0 +1,6 @@
+pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0
+pip install transformers==4.51.3 tokenizers==0.21.4 accelerate==1.10.1 safetensors==0.6.2
+pip install datasets==4.4.0
+pip install scikit-learn==1.7.2
+pip install matplotlib==3.10.7
+pip install ipdb
