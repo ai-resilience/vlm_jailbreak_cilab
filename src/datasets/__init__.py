345779731563ca79e506fc4c5bc19e8950d46ac0
Seol'Lab <83863024+SeolLab@users.noreply.github.com>
2025-11-11 12:32:02 +0900

    Remove JailBreakV28KDataset from dataset imports
    Removed references to JailBreakV28KDataset from dataset loading.

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -4,7 +4,6 @@ from .figstep import FigStepDataset
 from .strongreject import StrongREJECTDataset
 from .xstest import XSTestDataset
 from .advbench import AdvBenchDataset, HarmBenchDataset, SorryBenchDataset
-from .jailbreakv_28k import JailBreakV28KDataset
 from typing import Tuple, List, Optional
 
 
@@ -34,10 +33,6 @@ def load_dataset(
         'AdvBench': AdvBenchDataset,
         'HarmBench': HarmBenchDataset,
         'SorryBench': SorryBenchDataset,
-        'JailBreakV-28K': lambda **kw: JailBreakV28KDataset(source='huggingface', **kw),
-        'JailBreakV-28K_hf': lambda **kw: JailBreakV28KDataset(source='huggingface', **kw),
-        'JailBreakV-28K_github': lambda **kw: JailBreakV28KDataset(source='github', **kw),
-        'JailBreakV-28K_local': lambda **kw: JailBreakV28KDataset(source='local', **kw),
     }
     
     if name not in dataset_map:
@@ -63,7 +58,6 @@ __all__ = [
     'AdvBenchDataset',
     'HarmBenchDataset',
     'SorryBenchDataset',
-    'JailBreakV28KDataset',
     'load_dataset',
 ]
 
